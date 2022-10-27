c089d7fa0e1d437adfebc67e72d2d0409aed2557
Son Trinh <sontrinh@Sons-MacBook-Pro.local>
2022-10-28 05:04:37 +0700

    add stopper

--- index.js
+++ index.js
@@ -9,17 +9,13 @@ let node = 'http://95.217.121.243:2071/'
 
 // let client = RpcClient(node)
 
-const setupSocket = () => {
-    const socketClient = new WebsocketClient(node)
-    return socketClient
-}
+const socketClient = new WebsocketClient(node)
 
 let uptimes = []
 
 startSocket = () => {
     try {
-        let socket = setupSocket()
-        let stream = socket.listen({
+        let stream = socketClient.listen({
             "jsonrpc":"2.0", 
             "id": -1, 
             "method": "subscribe", 
@@ -35,12 +31,14 @@ startSocket = () => {
                 const check = checkMissBlock('junovalcons', valAddrs, 'junovalcons17rqsk5awpuynqask573sl25dgafm628rwvp7qq')
                 if (!check) {
                     const height = data.data.value.block.header.height
-                    uptimes.push({
+                    if (uptimes.length > 100) {
+                        uptimes.pop()
+                    }
+                    uptimes.unshift({
                         status: 'success',
                         height: height
                     })
                 }
-                console.log(check)
             }
         })
     }
@@ -51,44 +49,16 @@ startSocket = () => {
 
 startSocket()
 
-// app.get('/socket', (_, res) => {
-//     try {
-//         let socket = setupSocket()
-//         let stream = socket.listen({
-//             "jsonrpc":"2.0", 
-//             "id": -1, 
-//             "method": "subscribe", 
-//             "params": {
-//                 "query": "tm.event='NewBlock'"
-//             }
-//         })
-//         stream.map(data => data)
-//         stream.addListener({
-//             next: (data) => {
-//                 let signInfo = data.data.value.block.last_commit.signatures
-//                 let valAddrs = signInfo.map(sig => sig.validator_address)
-//                 const check = checkMissBlock('junovalcons', valAddrs, 'junovalcons17rqsk5awpuynqask573sl25dgafm628rwvp7qq')
-//                 if (check) {
-//                     uptimes.push({
-//                         status: 'success',
-//                         height: 2
-//                     })
-//                 }
-//                 console.log(check)
-//             }
-//         })
-//         res.json({
-//             data: uptimes
-//         })
-//     }
-//     catch (e) {
-//         res.send(e.message)
-//     }
-// })
-
-app.get('/uptime', (req, res) => {
+app.get('/uptime', (_, res) => {
     res.json({
         data: uptimes
+    })
+})
+
+app.get('/disconnect', (_, res) => {
+    socketClient.disconnect()
+    res.json({
+        status: 'success'
     })
 })
 
